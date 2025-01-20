77aad98d5928b6702c9353a56dfb181fbb29e16d
XiaoXiao-Woo <wxwsx1997@gmail.com>
2025-01-21 01:58:39 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+## datasets_searchpath_plugins
+
+SearchPath plugins are automatically discovered and enabled after installation. They provide configuration files (*.yaml) for datasets corresponding to specific tasks.
+
+### Key Features
+
+- **Automatic Configuration Discovery**: Users can easily discover and create configurations tailored to their needs.
+
+- **Hydra Integration**: Automatically extends Hydra with the keyword **"base"**, allowing for the addition of extra configurations within existing configuration groups.
+
+- **Configuration Priority**: Hydra prepends its configurations, ensuring they take precedence over any configurations they are intended to replace.
+
+- **Flexible Overrides**: Configurations can be replaced flexibly to suit various requirements.
+
+This setup simplifies the management of datasets and enhances the flexibility of configuration management.
+
+
+### Usage
+To install the package, run:
+```
+pip install -e .
+```
+
+Then, execute:
+
+```
+udl_share_conf /path/to/your/conf
+```
+
+In your your_work_cfg.yaml, look for base: xxx, which indicates that you can find the configuration file in the conf directory.
+
+```yaml
+defaults:
+  - base: xxx
+  - _self_
+```
+
+
+---
+
+Feel free to contact me:  
+**Email**: [Xiao.Wu@mbzuai.ac.ae](mailto:Xiao.Wu@mbzuai.ac.ae)
+
+## License & Copyright
+
+Copyright (c) 2025 Xiao Wu, MBZUAI and UESTC.
+
+This project is open sourced under GNU General Public License v3.0.
+
+
---
+++ setup.py
@@ -0,0 +1,41 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# type: ignore
+from setuptools import find_namespace_packages, setup, find_packages
+
+
+setup(
+    name="hydra-udl-searchpath-plugin",
+    version="0.0.1",
+    author="Xiao Wu",
+    author_email="wxwsx1997@gmail.com",
+    description="loading datasets conf using Hydra SearchPath plugin",
+    url="https://github.com/facebookresearch/hydra/",
+    packages=find_namespace_packages(include=["hydra_plugins.*"])
+    + find_packages(include=["customized.*"]),
+    classifiers=[
+        # Feel free to use another license.
+        "License :: OSI Approved :: MIT License",
+        # Hydra uses Python version and Operating system to determine
+        # In which environments to test this plugin
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "udl_share_conf = customized.link_yaml_files:link_yaml_files",
+        ],
+    },
+    install_requires=[
+        # consider pinning to a specific major version of Hydra to avoid unexpected problems
+        # if a new major version of Hydra introduces breaking changes for plugins.
+        # e.g: "hydra-core==1.0.*",
+        "hydra-core",
+    ],
+    # If this plugin is providing configuration files, be sure to include them in the package.
+    # See MANIFEST.in.
+    # For configurations to be discoverable at runtime, they should also be added to the search path.
+    include_package_data=True,
+)
